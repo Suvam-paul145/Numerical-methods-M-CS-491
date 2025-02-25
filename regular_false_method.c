cc7a3d2647ea1c183d9fc22d582c1163bc93ef89
Suvam-paul145 <suvampersonal555@gmail.com>
2025-02-25 11:14:50 +0530

    Implement regular falsi method with function definitions and user input for tolerance

--- regular_false_method.c
+++ regular_false_method.c
@@ -1,7 +1,44 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
-float c
-int main(){
+float f(float x)
+{
+    return f(x) = x * x - 2 * x - 7;
+}
 
+float regular_falsi(float a, float b, float tolerance)
+{
+    if (f(a) * f(b) > 0)
+    {
+        printf("Regular Falsi method is not applicable in that interval");
+        return 0;
+    }
+
+    else
+    {
+        while (b - a >= tolerance)
+        {
+            float c = (a * f(b) - b * f(a)) / (f(b) - f(a));
+
+            if ((c) == 0)
+            {
+                return f(c);
+            }
+            else if (f(a) * f(c) < 0)
+            {
+                b = c;
+            }
+            else
+            {
+                a = c;
+            }
+        }
+    }
+}
+
+int main()
+{   int tolerance;
+    printf("Please enter the tolerance value: ");
+    scanf("%f", &tolerance);
+    
 }
