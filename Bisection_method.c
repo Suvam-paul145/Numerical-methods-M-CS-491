6693edd3af7066ee4d195c58c155fc54d06ca6d1
Suvam-paul145 <suvampersonal555@gmail.com>
2025-03-03 19:35:44 +0530

    Refactor Bisection method for improved readability and correct function implementation

--- Bisection_method.c
+++ Bisection_method.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
 #include <math.h>
 
-float  f(float x) {
-	return (x*exp(x) -3);
+float f(float x)
+{
+	return (x * x - 2 * x + 3);
 }
 
-float bisection(float a, float b, float tolerance) {
+float bisection(float a, float b, float tolerance)
+{
 	float c;
-	if((f(a) * f(b)) >0) {
+	if ((f(a) * f(b)) > 0)
+	{
 		printf("Bisection methods are not appliable in that interval");
 		return 0;
 	}
 
-	while((b-a)>= tolerance) {
-		c=(a+b)/2;
+	while ((b - a) >= tolerance)
+	{
+		c = (a + b) / 2;
 
-		if(f(c) ==0) {
+		if (f(c) == 0)
+		{
 			return c;
-		} else if(f(a)*f(c) <0) {
-			b=c;
-		} else {
-			a=c;
+		}
+		else if (f(a) * f(c) < 0)
+		{
+			b = c;
+		}
+		else
+		{
+			a = c;
 		}
 	}
 	return c;
@@ -28,7 +37,6 @@ float bisection(float a, float b, float tolerance) {
 int main()
 {
 	float ans = bisection(1, 2, 0.0001);
-	printf("The ans is : %f",ans );
+	printf("The ans is : %f", ans);
 	return 0;
 }
-
