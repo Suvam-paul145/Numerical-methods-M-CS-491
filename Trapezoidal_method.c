c8c8c913318a9446579507ad76a18f9eb49ae246
Suvam-paul145 <suvampersonal555@gmail.com>
2025-03-03 19:31:59 +0530

    Refactor Trapezoidal method implementation for improved readability and accuracy

--- Trapezoidal_method.c
+++ Trapezoidal_method.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 
-double f(double x) {
+double f(double x)
+{
     // Calculate the square of the natural logarithm of x
-    return pow(log(x), 2);
+    return (log(x)) * (log(x));
 }
 
-double trapezoidal(double a, double b, int n) {
+double trapezoidal(double a, double b, int n)
+{
+    double sum = 0;
     double h = (b - a) / n;
-    double sum = (f(a) + f(b)) / 2.0;
-
-    for (int i = 1; i < n; i++) {
-        double x = a + i * h;
-        sum += f(x);
+    for (int i = 1; i < n; i++)
+    {
+        sum += f(a + i * h);
     }
-
-    return h * sum;
+    double result = sum + (f(a) + f(b)) / 2.0;
+    return h * result;
 }
 
-int main() {
+int main()
+{
     double a = 1.0, b = 1.5;
     int n = 10;
 
