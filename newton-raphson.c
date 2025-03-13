469e74e64045d5af09a1c5d6485156bee739c864
Suvam-paul145 <suvampersonal555@gmail.com>
2025-03-13 19:37:07 +0530

    Fix newton-raphson method implementation and return computed root value

--- newton-raphson.c
+++ newton-raphson.c
@@ -12,13 +12,17 @@ float derivative()
 }
 float newton_raphson(float x0, float tolerance)
 {
-    float h;
     while (derivative(x0) >= tolerance)
     {
+        x1 = f(x0) - f(x0) / derivative(x0);
+        x0 = x1;
     }
+    return x1;
 }
 
 int main()
 {
+    float x1;
     float ans = newton_raphson(1, 0.0001);
+    printf("The ans of the question is: ", ans);
 }
